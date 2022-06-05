4e179c5fe9fd8c436dbcfa91e7fc7e4d54d79324
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-06-05 15:33:58 +0200

    Time: 40 ms (74.65%), Space: 71.4 MB (50.42%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,69 +11,35 @@
 class Solution {
 public:
    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    int carry=0;
+    ListNode *ans = new ListNode();
+    ListNode *temp = ans;
 
-    ///1- create finalSum that reference to first list
-    ListNode * finalSum = l1;
 
-    ////2- create sumptr as a reference to finalSum
-    ListNode * sumptr = finalSum;
-    int carry =0;
-
-    ///3- iterate on l2
-    while(l2){
-        int tempSum=0;
-
-        /// to start add
-        ///1- case if sumPtr and l2 refer to the same node
-        if(sumptr==l2)
-            tempSum = sumptr->val+carry;
-        else
-            tempSum = sumptr->val+l2->val+carry;
-
-        ///2- in case the summation is greater than or equal to 10
-        if(tempSum>=10){
-            sumptr->val= tempSum%10;
-            carry= tempSum/10;
-        } else{
-            sumptr->val= tempSum;
-            carry=0;
+    while (l1||l2||carry){
+        int sum= 0;
+        if(l2){
+            sum+=l2->val;
+            l2 = l2->next;
         }
-
-        ///3- in case the next pointer is null in sumptr
-        if(sumptr->next== nullptr && l2->next!= nullptr)
-            sumptr->next = l2->next;
-        
-        sumptr = sumptr->next;
-        l2 =l2->next;
-
-
-
-    }
-    ///if sum ptr still has nodes
-    while (sumptr){
-        int tempSum=0;
-        tempSum = sumptr->val+carry;
-        if(tempSum>=10){
-            sumptr->val= tempSum%10;
-            carry= tempSum/10;
-        } else{
-            sumptr->val= tempSum;
-            carry=0;
+        if(l1){
+            sum+=l1->val;
+            l1 = l1->next;
         }
-        sumptr=sumptr->next;
-    }
-    ///in case of carry is not empty
+        sum+=carry;
 
-    if(carry){
-        ListNode* ptr = finalSum;
-        while (ptr->next!= nullptr){
-            ptr=ptr->next;
-        }
-        ptr->next= new ListNode(carry);
+        carry= sum/10;
+
+        ListNode *test = new ListNode(sum%10);
+        temp->next=test;
+        temp= temp->next;
 
     }
 
-    return  finalSum;
+
+    return ans->next;
+
+
 
 }
 };
