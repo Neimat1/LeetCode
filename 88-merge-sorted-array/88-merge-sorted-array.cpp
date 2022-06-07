3613567370c0a1f2b1aa8aead7d36c5c7ef8a63d
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-06-07 14:36:10 +0200

    Time: 7 ms (24.43%), Space: 9 MB (72.57%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
-void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    for(int i=0,j=m;i<n; i++,j++)
-        nums1[j] = nums2[i];
-
-    sort(nums1.begin(),nums1.end());
-}
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i=m,j=0;i<n+m;i++,j++){
+            nums1[i]=nums2[j];
+        }
+        
+        sort(nums1.begin(),nums1.end());
+        
+    }
 };
